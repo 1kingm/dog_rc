c1429e9ba7def6593ff06ee4b78c673c71f51830
1kingm <1993459689@qq.com>
2023-04-07 13:27:39 +0800

    修改双木桥，增加开始和结束条件

--- detect/double_bridge3.py
+++ detect/double_bridge3.py
@@ -1,40 +1,39 @@
 import cv2
 import numpy as np
 import math
-from typing import List
-from imutils import perspective
 
 video_name = 1
-cap = cv2.VideoCapture('1.mp4')
+cap = cv2.VideoCapture(video_name)
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))  # 膨胀腐蚀操作卷积核大小
-hsv_low = np.array([17,40,131])
-hsv_high = np.array([3,100,255])
+hsv_low = np.array([15, 42, 172])
+hsv_high = np.array([45, 116, 255])
+
 
 def length_line(dot1, dot2):
-    length = math.sqrt((dot1[0]-dot2[0])*(dot1[0]-dot2[0]) + (dot1[1]-dot2[1])*(dot1[1]-dot2[1]))
+    length = math.sqrt((dot1[0] - dot2[0]) * (dot1[0] - dot2[0]) + (dot1[1] - dot2[1]) * (dot1[1] - dot2[1]))
     return length
 
 
-def nei_abf(dot1, dot2): # 计算斜率和截距
-    Nei = (dot1[0] - dot2[0])/(dot1[1] - dot2[1]) # 斜率,
-    abf = dot1[0] - Nei*dot1[1] # 截距
-    return [Nei, abf] # x/y 默认斜率算法
-    
+def nei_abf(dot1, dot2):  # 计算斜率和截距
+    Nei = (dot1[0] - dot2[0]) / (dot1[1] - dot2[1])  # 斜率,
+    abf = dot1[0] - Nei * dot1[1]  # 截距
+    return [Nei, abf]  # x/y 默认斜率算法
 
-def rect_dot(frame, area1, area2, number): 
+
+def rect_dot(frame, area1, area2, number):
     rect1 = cv2.minAreaRect(area1)
     rect2 = cv2.minAreaRect(area2)
-    
-    approxCourve1= cv2.approxPolyDP(area1,10,closed = True)
+
+    approxCourve1 = cv2.approxPolyDP(area1, 10, closed=True)
     approxCourve1 = np.array(approxCourve1)
     approxCourve1 = np.squeeze(approxCourve1, axis=1)
-    approxCourve2= cv2.approxPolyDP(area2,10,closed = True)
+    approxCourve2 = cv2.approxPolyDP(area2, 10, closed=True)
     approxCourve2 = np.array(approxCourve2)
     approxCourve2 = np.squeeze(approxCourve2, axis=1)
-    
+
     # cv2.line(frame, approxCourve2[2], approxCourve2[1], (0, 255, 255), 2)
-    
-    if rect1[0][0] > rect2[0][0]: # 判断两个矩形位置，根据位置确定取的直线, rect1是右边的，rect2是左边的
+
+    if rect1[0][0] > rect2[0][0]:  # 判断两个矩形位置，根据位置确定取的直线, rect1是右边的，rect2是左边的
         if len(approxCourve1) == 4 and len(approxCourve2) == 4:
             if number == 1:
                 return length_line(approxCourve1[2], approxCourve1[3]) + length_line(approxCourve2[2], approxCourve2[1])
@@ -57,69 +56,67 @@ def rect_dot(frame, area1, area2, number):
             elif number == 2:
                 return [0, 0], [0, 0]
 
+
 while cap.isOpened():
     ret, frame = cap.read()
+    if not ret:
+        break
     frame = cv2.flip(frame, 1)
     frame = cv2.flip(frame, 0)
-    
+    print(1)
     frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(frame_hsv, hsv_low, hsv_high)
-    
+
     mask_median = cv2.medianBlur(mask, 3)
     mask_open = cv2.morphologyEx(mask_median, cv2.MORPH_OPEN, kernel)
     cv2.imshow('mask', mask_open)
     k = cv2.waitKey(1)
 
-    contours, _hierarchy = cv2.findContours(mask_open.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # 轮廓查找
+    contours, _hierarchy = cv2.findContours(mask_open.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # 轮廓查找
 
-    cnts = [] # 剔除部分之后的全部轮廓
-    for cnt in range(len(contours)): # 剔除部分轮廓
+    cnts = []  # 剔除部分之后的全部轮廓
+    for cnt in range(len(contours)):  # 剔除部分轮廓
         (x, y, w, h) = cv2.boundingRect(contours[cnt])
         if w > 10 and h > 30:
             cnts.append(contours[cnt])
-    
-    
-    area = [] # 轮廓面积
+
+    area = []  # 轮廓面积
     for cnt in cnts:
         area.append(cv2.contourArea(cnt))
-        
+
     # area为所有的cnt的面积
 
     area_sort = area
-    area_sort.sort(reverse=True) # 根据面积序降的area，排序结果
+    area_sort.sort(reverse=True)  # 根据面积序降的area，排序结果
 
     if len(cnts) < 2:
         continue
     print(len(cnts))
-    area1 = cnts[area.index(area_sort[0])] # 最大的轮廓
-    area2 = cnts[area.index(area_sort[1])] # 第二大的轮廓
-
+    area1 = cnts[area.index(area_sort[0])]  # 最大的轮廓
+    area2 = cnts[area.index(area_sort[1])]  # 第二大的轮廓
 
     [nei1, abf1], [nei2, abf2] = rect_dot(frame, area1=area1, area2=area2, number=2)
     if [nei1, abf1] == [0, 0] and [nei2, abf2] == [0, 0]:
         pass
-    #TODO 根据实际图像尺寸修改
+    # TODO 根据实际图像尺寸修改
     y_coors = np.arange(1, 1080)
     x_coors = []
 
     for y in y_coors:
-        x_coor = (y*nei1+abf1 +y*nei2+abf2)/2 
+        x_coor = (y * nei1 + abf1 + y * nei2 + abf2) / 2
         x_coors.append(x_coor)
-        cv2.circle(frame,(int(x_coor),int(y)),2,(0,0,255),-1)
-    
-    nei_median, _ = np.polyfit(y_coors, x_coors, 1) # 拟合直线
+        cv2.circle(frame, (int(x_coor), int(y)), 2, (0, 0, 255), -1)
+
+    nei_median, _ = np.polyfit(y_coors, x_coors, 1)  # 拟合直线
     print(nei_median)
-    if (nei_median<0.1) and (nei_median>-0.1): #TODO 判断直行的条件
+    if (nei_median < 0.1) and (nei_median > -0.1):  # TODO 判断直行的条件
         print("无需转向")
-        if rect_dot(frame, area1, area2, 1)  >= 40: #TODO根据最下的判断是否可以跳跃
+        if rect_dot(frame, area1, area2, 1) >= 40:  # TODO根据最下的判断是否可以跳跃
             print("跳跃")
         else:
             print("直行")
     else:
-        k_median = 1/nei_median # 斜率y/x
+        k_median = 1 / nei_median  # 斜率y/x
         print("转向")
     cv2.imshow('frame', frame)
     cv2.waitKey(1)
-    
-            
-            
--- just_do2.py
+++ just_do2.py
@@ -3,8 +3,8 @@ import numpy as np
 from imutils import perspective
 
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))  # 膨胀腐蚀操作卷积核大小
-hsv_low = np.array([0,50,81])
-hsv_high = np.array([50,91,255])
+hsv_low = np.array([0, 50, 81])
+hsv_high = np.array([50, 91, 255])
 
 
 def doubel_bridge(frame):
@@ -18,35 +18,34 @@ def doubel_bridge(frame):
     try:
         contours, _hierarchy = cv2.findContours(mask_open.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         for i in contours:
-            frame = cv2.drawContours(frame,i,-1,(255,0,0),2)
+            frame = cv2.drawContours(frame, i, -1, (255, 0, 0), 2)
 
         area = [-1, -2]
         for cnt in contours:
             area.append(cv2.contourArea(cnt))
         area_sort = area
         area_sort.sort()
-        area1 = contours[contours.index(area_sort[-1])] 
+        area1 = contours[contours.index(area_sort[-1])]
         # 以下为计算距离
         rect1 = cv2.minAreaRect(area1)  # 最小外接矩阵
         area2 = contours[contours.index(area_sort[-2])]
         rect2 = cv2.minAreaRect(contours[area2])  # 最小外接矩阵
-        box1 = np.int0(cv2.boxPoints(rect1)) # 四个顶点并取整数
-        
-        box2 = np.int0(cv2.boxPoints(rect2)) # 四个顶点并取整数
+        box1 = np.int0(cv2.boxPoints(rect1))  # 四个顶点并取整数
+
+        box2 = np.int0(cv2.boxPoints(rect2))  # 四个顶点并取整数
         box1_point = perspective.order_points(box1)
         box1_dot1 = box1_point[2]
         box1_dot2 = box1_point[3]
-        length1 = math.sqrt(pow((box1_dot1[0]-box1_dot2[0]), 2) + pow((box1_dot1[1]-box1_dot2[1]), 2))
-        
+        length1 = math.sqrt(pow((box1_dot1[0] - box1_dot2[0]), 2) + pow((box1_dot1[1] - box1_dot2[1]), 2))
+
         rect2 = cv2.minAreaRect(contours[area2])  # 最小外接矩阵
-        box2 = np.int0(cv2.boxPoints(rect2)) # 四个顶点并取整数
+        box2 = np.int0(cv2.boxPoints(rect2))  # 四个顶点并取整数
         box2_point = perspective.order_points(box2)
         box2_dot1 = box2_point[2]
         box2_dot2 = box2_point[3]
-        length2 = math.sqrt(pow((box2_dot1[0]-box2_dot2[0]), 2) + pow((box2_dot1[1]-box2_dot2[1]), 2))
-        
-                
-        y = np.array([100, 300])  #  [0-300]····
+        length2 = math.sqrt(pow((box2_dot1[0] - box2_dot2[0]), 2) + pow((box2_dot1[1] - box2_dot2[1]), 2))
+
+        y = np.array([100, 300])  # [0-300]····
         x_prime = list()  # 列表
         k = 0
         for cnt in area1, area2:  # 可用轮廓
@@ -54,7 +53,7 @@ def doubel_bridge(frame):
             y_coord = cnt[:, 1]  # y
             param = np.polyfit(y_coord, x_coord, 1)  # 已有轮廓拟合直线
             x_prime.append(np.polyval(param, y))  # 用y带入已知直线得到x_prime,两个轮廓
-            
+
         x_prime = np.array(x_prime)
         x_average = np.average(x_prime, axis=0)  # 所有轮廓x平均
 
@@ -62,17 +61,13 @@ def doubel_bridge(frame):
         start_point = [100, int(np.polyval(mid_param, 100))]
         end_point = [300, int(np.polyval(mid_param, 300))]
         cv2.line(frame, start_point, end_point, (0, 255, 0), 2)
-        
+
         try:
-            k = (start_point[1] - end_point[1])/(start_point[0] - end_point[0])  
+            k = (start_point[1] - end_point[1]) / (start_point[0] - end_point[0])
         except:
             k = 0
             if length1 + length2 >= 40:
-                k = 10000 # 可以跳跃
+                k = 10000  # 可以跳跃
     except:
         print("contours")
         pass
-            
-    
-    
-    
